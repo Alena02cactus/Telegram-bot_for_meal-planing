571f83d1214acea6f636be8e6abe7532d1414954
Alena02cactus <129507128+Alena02cactus@users.noreply.github.com>
2025-07-28 22:00:25 +0000

    update config.go

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,26 @@
 package config
+
+import (
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	TelegramToken string `mapstructure:"TELEGRAM_TOKEN"`
+	DatabaseURL   string `mapstructure:"DATABASE_URL"`
+}
+
+func Load() (*Config, error) {
+	viper.SetConfigFile(".env")
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, err
+	}
+
+	var cfg Config
+	if err := viper.Unmarshal(&cfg); err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
+}
